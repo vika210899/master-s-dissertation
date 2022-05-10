916ffe71e14faff1333d43b40c964fb7f85455a3
Vika Vika <vika2108991@gmail.com>
2022-05-10 16:23:40 +0300

    update 1-grams.py

--- 1-grams.py
+++ 1-grams.py
@@ -1,125 +1,23 @@
 # decomposition into 1-grams
 import time
-# from spacy.lang.ru import Russian
-import textacy
 import spacy
-
-# start_SpaCy = time.time()
-# print(time.time() - start_SpaCy, ' - SpaCy')
+import functional
 
 text = "Пушкин неоднократно писал о своей родословной в стихах и прозе; он видел в своих предках образец истинной «аристократии», древнего рода, честно служившего отечеству, но не снискавшего благосклонности правителей и «гонимого». Я-то из-за угла вышел 7 апреля."
-# text = "London is the capital and most populous city of England and the United Kingdom.  Standing on the River Thames in the south east of the island of Great Britain, London has been a major settlement for two millennia. It was founded by the Romans, who named it Londinium."
-
-# nlp = Russian()
-# nlp = spacy.load('en_core_web_lg')
 nlp = spacy.load('ru_core_news_sm')
-
-# 0.	Исходный текст поступает на вход
 doc = nlp(text)
 
-# 1. Сегментация + 2. Токенизация + 4. Удаление стоп-слов + 4* Удаление цифр
-ngrams = [ngram for n in range(1, 4)
-          for ngram in textacy.extract.ngrams(doc, n)]  # , filter_stops=True, filter_nums=True)]
-print(ngrams)
+# time_start = time.time()
 
-# result_SpaCy = list(textacy.extract.ngrams(doc, 1, filter_stops=True, filter_nums=True))
-# print(result_SpaCy) 
+result_SpaCy = functional.tokenize_and_clean(doc, 1)
+# functional.get_ents(doc)
+# functional.get_lemma(doc, result_SpaCy)  
+# functional.get_pos(doc, result_SpaCy)  
+# functional.get_relation(doc, result_SpaCy)
+# functional.get_role(doc, result_SpaCy)
+# functional.display_roles(doc)
+# functional.get_synonymss(doc)
+# terms = functional.nouns_ngrams(doc)
+# functional.all_together(doc, result_SpaCy)
 
-# # 2*.	*Промежуточный необязательный шаг* – определение частей речи
-# print("Часть речи: ")
-# for token in doc:
-#     token_text = token.text
-#     token_pos = token.pos_
-#     print(token_text, ' - ', token_pos)
-# # либо 
-# print({token.text: token.pos_ for token in doc})
-
-# # 3.	Лемматизация: токены приводятся к начальной форме
-# print("Лемматизация: ")
-# for token in doc:
-#     token_text = token.text
-#     token_lemma = token.lemma_
-#     print(token_text, ' - ', token_lemma)
-# # либо
-# print({token.text: token.lemma_ for token in doc})
-# # либо
-# print(' '.join([token.lemma_ for token in doc]))
-
-# # 4.	Удаление стоп-слов (добавлено в п.1), слишком коротких слов
-# # ----------------------------------
-
-# # 4*.	*Промежуточный необязательный шаг* – удаление цифр (добавлено в п.1)
-
-# # 4**.	*Промежуточный необязательный шаг* – парсинг зависимостей
-# print("Зависимости от других слов: ")
-# for token in doc:
-#     token_text = token.text
-#     token_head = token.head.text
-#     print(token_text, ' - ', token_head)
-
-# # 4**.	*Промежуточный необязательный шаг* – роль в предложении
-# print("Роль в предложении: ")
-# for token in doc:
-#     token_text = token.text
-#     token_dep = token.dep_
-#     print(token_text, ' - ', token_dep)
-
-# # + отрисовка зависимостей слов в предложении
-# from spacy import displacy
-# from pathlib import Path
-# svg = displacy.render(doc, style="dep")
-# output_path = Path("images/sentence.svg")
-# output_path.open("w", encoding="utf-8").write(svg)
-
-# 4***.	*Промежуточный необязательный шаг* – группы существительных
-# ----------------------------------
-
-# # 4****.	*Промежуточный необязательный шаг* – поиск именованных сущностей 
-# распознаются и простые имена и фамилии!!!!!!!!!!!!!!
-# for ent in doc.ents:
-#     print(ent.text, ent.label_)
-# # либо
-# for entity in doc.ents:
-#     print(f"{entity.text} ({entity.label_})")
-
-# 4*****.	Можно еще внести распознавание семантической близости, но так как все кластеры будут похожи 
-# ----------------------------------
-
-
-
-
-
-
-
-
-
-
-
-# не работает для русского
-
-# # Извлечение фрагментов
-# noun_chunks = textacy.extract.noun_chunks(doc, min_freq=3)
-# print([noun_chunk for noun_chunk in noun_chunks])
-# # Перевод в нижний регистр
-# noun_chunks = map(str, noun_chunks)
-# noun_chunks = map(str.lower, noun_chunks)
-# print(noun_chunks)
-
-# # вывод всех фрагментов, состоящих из 2 слов и более
-# for noun_chunk in set(noun_chunks):
-#     if len(noun_chunk.split(" ")) > 1:
-#         print(noun_chunk)
-
-
-
-
-
-# for token in doc:
-#     token_text = token.text
-#     token_pos = token.pos_
-#     token_dep = token.dep_
-#     token_head = token.head.text
-#     token_lemma = token.lemma_
-#     print(f"{token_text:<16}{token_lemma:<16}{token_pos:<10}" \
-#           f"{token_dep:<10}{token_head:<12}")
-
+# print(time.time() - time_start)
---
+++ functional.py
@@ -0,0 +1,129 @@
+import textacy
+from spacy import displacy
+from pathlib import Path
+from functools import partial
+import textacy.resources
+import time
+
+# 1. Сегментация + 2. Токенизация + 4. Удаление стоп-слов + 4* Удаление цифр
+def tokenize_and_clean(document, n):
+    result_SpaCy = list(
+        textacy.extract.ngrams(document, n, filter_stops=True, filter_nums=True, include_pos={"NOUN", "ADJ"})
+    )
+    print(result_SpaCy)
+    return result_SpaCy
+
+# *** сделать токенизацию с цифрами
+# *** сделать токенизацию со стоп словами
+
+# ngramsss = list(textacy.extract.ngrams, n=2, include_pos={"NOUN", "ADJ"})
+# print(ngramsss)
+
+
+# 2*. определение частей речи
+def get_pos(doc, result):
+    # print("Часть речи: ")
+    # for token in doc:
+    #     for token_ in result:
+    #         token_text = token.text
+    #         if token_text == token_.text:
+    #             token_pos = token.pos_
+    #             print(token_text, " - ", token_pos)
+
+# либо
+    print({token.text: token.pos_ for token in doc for token_ in result if token.text == token_.text})
+
+# 3.	Лемматизация
+def get_lemma(doc, result):
+    # print("Лемматизация: ")
+    # for token in doc:
+    #     for token_ in result:
+    #         token_text = token.text
+    #         if token_text == token_.text:
+    #             token_lemma = token.lemma_
+    #             print(token_text, " - ", token_lemma)
+    # либо
+    print({token.text: token.lemma_ for token in doc for token_ in result if token.text == token_.text})
+    # # либо
+    # print(' '.join([token.lemma_ for token in doc]))
+
+# 4**. парсинг зависимостей
+def get_relation(doc, result):
+    print("Зависимости от других слов: ")
+    # for token in doc:
+    #     token_text = token.text
+    #     token_head = token.head.text
+    #     print(token_text, " - ", token_head)
+    print({token.text: token.head.text for token in doc for token_ in result if token.text == token_.text})
+
+
+# 4**. роль в предложении
+def get_role(doc, result):
+    print("Роль в предложении: ")
+    # for token in doc:
+    #     token_text = token.text
+    #     token_dep = token.dep_
+    #     print(token_text, " - ", token_dep)
+    print({token.text: token.dep_ for token in doc for token_ in result if token.text == token_.text})
+
+
+# # + отрисовка зависимостей слов в предложении
+def display_roles(doc):
+    svg = displacy.render(doc, style="dep")
+    output_path = Path("images/sentence.svg")
+    output_path.open("w", encoding="utf-8").write(svg)
+
+
+# 4***.	*Промежуточный необязательный шаг* – n-граммы с существительным
+def nouns_ngrams(doc):
+    terms = list(
+        textacy.extract.terms(
+            doc,
+            ngs=partial(
+                textacy.extract.ngrams,
+                n=2,
+                filter_stops=True,
+                filter_nums=True,
+                include_pos={"NOUN", "ADJ"},
+            ),
+        )
+    )  # , ents=partial(textacy.extract.entities, include_types={"PER", "ORG", "GPE", "LOC"}))) #, dedupe=True))
+    print(terms)
+    return terms
+
+
+# 4****. поиск именованных сущностей
+# распознаются и простые имена и фамилии!!!!!!!!!!!!!! д.б. написаны в определенном порядке
+def get_ents(doc):
+    for ent in doc.ents:
+        print(ent.text, ent.label_)
+    # либо
+    # for entity in doc.ents:
+    #     print(f"{entity.text} ({entity.label_})")
+    # либо
+    # ent_Spacy = list(textacy.extract.entities(doc))
+    # return(ent_Spacy)
+
+# 4*****.	Можно еще внести распознавание семантической близости, но так как все кластеры будут похожи
+def get_synonymss(doc):
+    rs = textacy.resources.ConceptNet()
+    # rs.download()
+    syn = rs.get_synonyms(term=doc[15].lemma_, lang="ru", sense="n")
+    print(syn)
+    return syn
+
+
+# все вместе
+def all_together(doc, result_tok):
+    for token in doc:
+        for token_ in result_tok:
+            token_text = token.text
+            if token_text == token_.text:
+                token_pos = token.pos_
+                token_dep = token.dep_
+                token_head = token.head.text
+                token_lemma = token.lemma_
+                print(
+                    f"{token_text:<16}{token_lemma:<16}{token_pos:<10}"
+                    f"{token_dep:<10}{token_head:<12}"
+                )
