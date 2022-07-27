82bfdb775bfc8d8aafd3b2bf05266e5467b20ab7
Viktoria Tereshchenko <vika210899@mail.ru>
2022-07-27 17:35:31 +0300

    comments for assessment/time

--- assessment/time/assessment.py
+++ assessment/time/assessment.py
@@ -4,24 +4,24 @@ import xlsx_in_txt
 import time
 import spacy
 
-st_txt = time.time()
-st_txt_ns = time.time_ns()
-test_text = xlsx_in_txt.read_data()
-test_text = test_text[500:600]
-fin_txt = time.time() - st_txt
-fin_txt_ns = time.time_ns() - st_txt_ns
+st_txt = time.time()  # фиксируем начало чтения данных из xlsx в секундах
+st_txt_ns = time.time_ns()  # фиксируем начало чтения данных из xlsx в наносекундах
+test_text = xlsx_in_txt.read_data()  # запускаем чтение данных из файла xlsx
+test_text = test_text[500:600]  # выбираем диапазон строк, которые хотим обработать (номера элементов списка с данными начинается с нуля)
+fin_txt = time.time() - st_txt  # фиксируем конец чтения данных из xlsx в секундах
+fin_txt_ns = time.time_ns() - st_txt_ns  # фиксируем конец чтения данных из xlsx в наносекундах
 
 
-st_df = time.time()
-nlp = spacy.load('ru_core_news_sm')
-
-df = pd.DataFrame({  # 'Nlp_load': [TextProcessing().tok_only(sent, nlp)[0] for sent in test_text],
-    # 'Doc_load': [TextProcessing().tok_only(sent, nlp)[1] for sent in test_text],
-    'Tok_only': [TextProcessing().tok_only(sent, nlp) for sent in test_text],
-    'Tok_filter': [TextProcessing().tok_filter(sent, nlp) for sent in test_text],
-    'Tok_ents': [TextProcessing().tok_ents(sent, nlp) for sent in test_text],
-    'Tok_filter_ents': [TextProcessing().tok_filter_ents(sent, nlp) for sent in test_text],
-    'Tok_checkPOS': [TextProcessing().tok_checkPOS(sent, nlp) for sent in test_text],
-    'Tok_filter_checkPOS': [TextProcessing().tok_filter_checkPOS(sent, nlp) for sent in test_text]})
-fin_df = time.time() - st_df
-df.to_excel('result_tests/result_of_tests_4_t.xlsx', index=False)
+st_df = time.time()  # фиксируем начало построения датафрейма в секундах
+nlp = spacy.load('ru_core_news_sm')  # загружаем модель spacy для русского языка
+# формируем датафрейм [одна строка кода ниже = один столбец формируемой таблицы]
+df = pd.DataFrame({  # 'Nlp_load': [TextProcessing().tok_only(sent, nlp)[0] for sent in test_text],  # здесь время загрузки модели spacy для русского языка
+    # 'Doc_load': [TextProcessing().tok_only(sent, nlp)[1] for sent in test_text],  # время формирования объекта doc
+    'Tok_only': [TextProcessing().tok_only(sent, nlp) for sent in test_text],  # время работы токенизатора Tok_only
+    'Tok_filter': [TextProcessing().tok_filter(sent, nlp) for sent in test_text],  # время работы токенизатора Tok_filter
+    'Tok_ents': [TextProcessing().tok_ents(sent, nlp) for sent in test_text],  # время работы токенизатора Tok_ents
+    'Tok_filter_ents': [TextProcessing().tok_filter_ents(sent, nlp) for sent in test_text],  # время работы токенизатора Tok_filter_ents
+    'Tok_checkPOS': [TextProcessing().tok_checkPOS(sent, nlp) for sent in test_text],  # время работы токенизатора Tok_checkPOS
+    'Tok_filter_checkPOS': [TextProcessing().tok_filter_checkPOS(sent, nlp) for sent in test_text]})  # время работы токенизатора Tok_filter_checkPOS
+fin_df = time.time() - st_df  # фиксируем конец построения датафрейма в секундах
+df.to_excel('result_tests/result_of_tests_4_t.xlsx', index=False)  # записываем датафрейм в эксель
--- assessment/time/text_processing.py
+++ assessment/time/text_processing.py
@@ -4,77 +4,86 @@ import functional
 import spacy
 import time
 
-
+# время работы токенизаторов считается с процессом построения объекта doc, потому что иначе многие значения времени были бы 0.0 (даже в микросекундах),
+# что не было наглядными значениями для исследования. То есть в действительности время работы еще меньше,
+# чем в исследованиях у меня в дипломной работе 
 class TextProcessing:
 
+    # Токенизация без фильтрации
     def tok_only(self, raw_text, nlp):
-        # time_nlp = time.time()
-        # nlp = spacy.load('ru_core_news_sm')
-        # fin_time_nlp = time.time() - time_nlp
+        # time_nlp = time.time()  # фиксируем время начала загрузки модели ru_core_news_sm
+        # nlp = spacy.load('ru_core_news_sm')  # загружаем модель
+        # fin_time_nlp = time.time() - time_nlp  # фиксируем время конца загрузки
+        
+        # считаем время выполнения токенизации
+        time_start = time.time()  # запоминаем время начала декомпозиции
+        # time_doc = time.time()  # запоминаем время начала формирования объекта doc
+        doc = nlp(raw_text)  # создаем doc
+        # fin_time_doc = time.time() - time_doc  # запоминаем время конца формирования doc и считаем продолжительность процедуры формирования doc
 
-        time_start = time.time()
-        # time_doc = time.time()
-        doc = nlp(raw_text)
-        # fin_time_doc = time.time() - time_doc
+        n = 4  # задаем тип элемента декомпозиции
+        result_tok = tok.tokenize_only(doc, n)  # запускаем декомпозицию исхдного текста и записываем результат в result_tok
+        # result_tok = functional.get_lemma(result_tok, n)  # лемматизируем список токенов
+        # result_tok = functional.remove_spec_stop_words(result_tok, n)  # удаляем специализированные стоп слова
+        time_finish = time.time() - time_start  # фиксируем время завершения процессов декомпозиции
+        return time_finish  # возвращаем время работы декомпозитора
 
-        n = 4
-        result_tok = tok.tokenize_only(doc, n)
-        # result_tok = functional.get_lemma(result_tok, n)
-        # result_tok = functional.remove_spec_stop_words(result_tok, n)
-        time_finish = time.time() - time_start
-        return time_finish
-
+    # Токенизация с фильтрацией стоп-слов и цифр
     def tok_filter(self, raw_text, nlp):
-        time_start = time.time()
-        doc = nlp(raw_text)
-        n = 4
-        result_tok = tok.tokenize_filter(doc, n)
-        # result_tok = functional.get_lemma(result_tok, n)
-        # result_tok = functional.remove_spec_stop_words(result_tok, n)
+        time_start = time.time()  # запоминаем время начала декомпозиции
+        doc = nlp(raw_text)  # создаем объект doc
+        n = 4  # задаем тип элемента декомпозиции
+        result_tok = tok.tokenize_filter(doc, n)  # запускаем декомпозицию исхдного текста и записываем результат в result_tok
+        # result_tok = functional.get_lemma(result_tok, n)  # лемматизируем список токенов
+        # result_tok = functional.remove_spec_stop_words(result_tok, n)  # удаляем специализированные стоп слова
 
-        time_finish = time.time() - time_start
-        return time_finish
+        time_finish = time.time() - time_start  # фиксируем время завершения процессов декомпозиции
+        return time_finish  # возвращаем время работы декомпозитора
 
+    # Токенизация с фильтрацией стоп-слов, цифр и именных сущностей
     def tok_filter_ents(self, raw_text, nlp):
-        time_start = time.time()
-        doc = nlp(raw_text)
-        n = 4
-        result_tok = tok.tokenize_filter_ents(doc, n)
-        # result_tok = functional.get_lemma(result_tok, n)
-        # result_tok = functional.remove_spec_stop_words(result_tok, n)
+        time_start = time.time()  # запоминаем время начала декомпозиции
+        doc = nlp(raw_text)  # создаем объект doc
+        n = 4  # задаем тип элемента декомпозиции
+        result_tok = tok.tokenize_filter_ents(doc, n)  # запускаем декомпозицию исхдного текста и записываем результат в result_tok
+        # result_tok = functional.get_lemma(result_tok, n)  # лемматизируем список токенов
+        # result_tok = functional.remove_spec_stop_words(result_tok, n)  # удаляем специализированные стоп слова
 
-        time_finish = time.time() - time_start
-        return time_finish
+        time_finish = time.time() - time_start  # фиксируем время завершения процессов декомпозиции
+        return time_finish  # возвращаем время работы декомпозитора
 
+    # Токенизация с фильтрацией именных сущностей
     def tok_ents(self, raw_text, nlp):
-        time_start = time.time()
-        doc = nlp(raw_text)
-        n = 4
-        result_tok = tok.tokenize_ents(doc, n)
-        # result_tok = functional.get_lemma(result_tok, n)
-        # result_tok = functional.remove_spec_stop_words(result_tok, n)
+        time_start = time.time()  # запоминаем время начала декомпозиции
+        doc = nlp(raw_text)  # создаем объект doc
+        n = 4  # задаем тип элемента декомпозиции
+        result_tok = tok.tokenize_ents(doc, n)  # запускаем декомпозицию исхдного текста и записываем результат в result_tok
+        # result_tok = functional.get_lemma(result_tok, n)  # лемматизируем список токенов
+        # result_tok = functional.remove_spec_stop_words(result_tok, n)  # удаляем специализированные стоп слова
 
-        time_finish = time.time() - time_start
-        return time_finish
+        time_finish = time.time() - time_start  # фиксируем время завершения процессов декомпозиции
+        return time_finish  # возвращаем время работы декомпозитора
 
+    # Токенизация с фильтрацией стоп-слов и цифр и отбором определенных частей речи
     def tok_filter_checkPOS(self, raw_text, nlp):
-        time_start = time.time()
-        doc = nlp(raw_text)
-        n = 4
-        result_tok = tok.tokenize_filter_checkPOS(doc, n)
-        # result_tok = functional.get_lemma(result_tok, n)
-        # result_tok = functional.remove_spec_stop_words(result_tok, n)
+        time_start = time.time()  # запоминаем время начала декомпозиции
+        doc = nlp(raw_text)  # создаем объект doc
+        n = 4  # задаем тип элемента декомпозиции
+        result_tok = tok.tokenize_filter_checkPOS(doc, n)  # запускаем декомпозицию исхдного текста и записываем результат в result_tok
+        # result_tok = functional.get_lemma(result_tok, n)  # лемматизируем список токенов
+        # result_tok = functional.remove_spec_stop_words(result_tok, n)  # удаляем специализированные стоп слова
 
-        time_finish = time.time() - time_start
-        return time_finish
+        time_finish = time.time() - time_start  # фиксируем время завершения процессов декомпозиции
+        return time_finish  # возвращаем время работы декомпозитора
 
+    # Токенизация без фильтрации и с отбором определенных частей речи
     def tok_checkPOS(self, raw_text, nlp):
-        time_start = time.time()
-        doc = nlp(raw_text)
-        n = 4
-        result_tok = tok.tokenize_checkPOS(doc, n)
-        # result_tok = functional.get_lemma(result_tok, n)
-        # result_tok = functional.remove_spec_stop_words(result_tok, n)
+        time_start = time.time()  # запоминаем время начала декомпозиции
+        doc = nlp(raw_text)  # создаем объект doc
+        n = 4  # задаем тип элемента декомпозиции
+        result_tok = tok.tokenize_checkPOS(doc, n)  # запускаем декомпозицию исхдного текста и записываем результат в result_tok
+        # result_tok = functional.get_lemma(result_tok, n)  # лемматизируем список токенов
+        # result_tok = functional.remove_spec_stop_words(result_tok, n)  # удаляем специализированные стоп слова
 
-        time_finish = time.time() - time_start
-        return time_finish
+        time_finish = time.time() - time_start  # фиксируем время завершения процессов декомпозиции
+        return time_finish  # возвращаем время работы декомпозитора
